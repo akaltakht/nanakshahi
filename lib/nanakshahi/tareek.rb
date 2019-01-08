0dcfce1c20ed77448592cbdf8f9d9919da409646
Arvinder Singh <info@pippallabs.com>
2019-01-08 13:48:31 -0800

    🔥 Refactoring
    Refactored nanakshahi_months into a class method

--- lib/nanakshahi/tareek.rb
+++ lib/nanakshahi/tareek.rb
@@ -17,6 +17,28 @@ module Nanakshahi
 
     class Error < StandardError; end
 
+    def self.date_grid(gyear)
+      # #
+      # The months follow the same index as MONTHS constant
+      #   therefore although in Gregorian calendar Poh is first
+      #   month of the nanakshahi year, it is at index 9
+      #   sort of a merry-go-round.
+      [
+        (Date.new(gyear, 3, 14)..Date.new(gyear, 4, 13)).to_a,
+        (Date.new(gyear, 4, 14)..Date.new(gyear, 5, 14)).to_a,
+        (Date.new(gyear, 5, 15)..Date.new(gyear, 6, 14)).to_a,
+        (Date.new(gyear, 6, 15)..Date.new(gyear, 7, 15)).to_a,
+        (Date.new(gyear, 7, 16)..Date.new(gyear, 8, 15)).to_a,
+        (Date.new(gyear, 8, 16)..Date.new(gyear, 9, 14)).to_a,
+        (Date.new(gyear, 9, 15)..Date.new(gyear, 10, 14)).to_a,
+        (Date.new(gyear, 10, 15)..Date.new(gyear, 11, 13)).to_a,
+        (Date.new(gyear, 11, 14)..Date.new(gyear, 12, 13)).to_a,
+        (Date.new(gyear, 12, 14)..Date.new(gyear, 12, 31)).to_a.concat((Date.new(gyear, 1, 1)..Date.new(gyear, 1, 12)).to_a),
+        (Date.new(gyear, 1, 13)..Date.new(gyear, 2, 11)).to_a,
+        (Date.new(gyear, 2, 12)..Date.new(gyear, 3, 13)).to_a,
+      ]
+    end
+
     MONTHS = %w(Chet Vaisakh Jeth Harh Sawan Bhadon Asu Katik Maghar Poh Magh Phagun)
     MONTHS_PA = %w(ਚੇਤ ਵੈਸਾਖ ਜੇਠ ਹਾੜ ਸਾਵਣ ਭਾਦੋਂ ਅੱਸੂ ਕੱਤਕ ਮੱਘਰ ਪੋਹ ਮਾਘ ਫੱਗਣ)
 
@@ -58,21 +80,7 @@ module Nanakshahi
         gyear = year + 1469
       end
 
-      nanakshahi_months = [
-        (Date.new(gyear, 3, 14)..Date.new(gyear, 4, 13)).to_a,
-        (Date.new(gyear, 4, 14)..Date.new(gyear, 5, 14)).to_a,
-        (Date.new(gyear, 5, 15)..Date.new(gyear, 6, 14)).to_a,
-        (Date.new(gyear, 6, 15)..Date.new(gyear, 7, 15)).to_a,
-        (Date.new(gyear, 7, 16)..Date.new(gyear, 8, 15)).to_a,
-        (Date.new(gyear, 8, 16)..Date.new(gyear, 9, 14)).to_a,
-        (Date.new(gyear, 9, 15)..Date.new(gyear, 10, 14)).to_a,
-        (Date.new(gyear, 10, 15)..Date.new(gyear, 11, 13)).to_a,
-        (Date.new(gyear, 11, 14)..Date.new(gyear, 12, 13)).to_a,
-        (Date.new(gyear, 12, 14)..Date.new(gyear, 12, 31)).to_a.concat((Date.new(gyear, 1, 1)..Date.new(gyear, 1, 12)).to_a),
-        (Date.new(gyear, 1, 13)..Date.new(gyear, 2, 11)).to_a,
-        (Date.new(gyear, 2, 12)..Date.new(gyear, 3, 13)).to_a,
-      ]
-
+      nanakshahi_months = self.class.date_grid(gyear)
       nanakshahi_months[month - 1][day - 1]
     end
 
@@ -95,25 +103,7 @@ module Nanakshahi
         nyear = gyear - 1468
       end
 
-      # #
-      # The months follow the same index as MONTHS constant
-      #   therefore although in Gregorian calendar Poh is first
-      #   month of the nanakshahi year, it is at index 9
-      #   sort of a merry-go-round.
-      nanakshahi_months = [
-        (Date.new(gyear, 3, 14)..Date.new(gyear, 4, 13)).to_a,
-        (Date.new(gyear, 4, 14)..Date.new(gyear, 5, 14)).to_a,
-        (Date.new(gyear, 5, 15)..Date.new(gyear, 6, 14)).to_a,
-        (Date.new(gyear, 6, 15)..Date.new(gyear, 7, 15)).to_a,
-        (Date.new(gyear, 7, 16)..Date.new(gyear, 8, 15)).to_a,
-        (Date.new(gyear, 8, 16)..Date.new(gyear, 9, 14)).to_a,
-        (Date.new(gyear, 9, 15)..Date.new(gyear, 10, 14)).to_a,
-        (Date.new(gyear, 10, 15)..Date.new(gyear, 11, 13)).to_a,
-        (Date.new(gyear, 11, 14)..Date.new(gyear, 12, 13)).to_a,
-        (Date.new(gyear, 12, 14)..Date.new(gyear, 12, 31)).to_a.concat((Date.new(gyear, 1, 1)..Date.new(gyear, 1, 12)).to_a),
-        (Date.new(gyear, 1, 13)..Date.new(gyear, 2, 11)).to_a,
-        (Date.new(gyear, 2, 12)..Date.new(gyear, 3, 13)).to_a,
-      ]
+      nanakshahi_months = date_grid(gyear)
 
       row = nanakshahi_months.detect { |month| month.include?(gdate) }
       nday, nmonth = [row.index(gdate), nanakshahi_months.index(row)]
