002fe77b2388c0fd90b5cb43fa1c36c16ac35fd7
Arvinder Singh <info@pippallabs.com>
2019-01-08 20:02:32 -0800

    🎨 Refactored code
    Epoch made to a constant which it naturally is

--- lib/nanakshahi/utils.rb
+++ lib/nanakshahi/utils.rb
@@ -1,23 +1,23 @@
 require "date"
 
 module Utils
+  NANAKSHAHI_EPOCH_IN_GREGORIAN = Date.new(1469, 3, 14)
   # #
   # classs method Gregorian to Nanakshahi date
   # returns a Nanakshahi date object
   def from_gregorian(gyear, gmonth, gday)
     # Start of time -  1 Chet, 1 Nanakshahi
-    nanakshahi_epoch_in_gregorian = Date.new(1469, 3, 14)
     gdate = Date.new(gyear, gmonth, gday)
 
     # Implement ਧੁੰਦਕਾਲ(Before Nanak - from Bhai Gurdas's vaar) later
-    # Any date prior to nanakshahi_epoch_in_gregorian returns nil for now
-    # return nil if gdate < nanakshahi_epoch_in_gregorian
+    # Any date prior to NANAKSHAHI_EPOCH_IN_GREGORIAN returns nil for now
+    # return nil if gdate < NANAKSHAHI_EPOCH_IN_GREGORIAN
 
     # Before Chet 1, it is previous year in Nanakshahi
-    if (gmonth <= nanakshahi_epoch_in_gregorian.month) && (gday < nanakshahi_epoch_in_gregorian.day)
-      nyear = gyear - 1469
+    if (gmonth <= NANAKSHAHI_EPOCH_IN_GREGORIAN.month) && (gday < NANAKSHAHI_EPOCH_IN_GREGORIAN.day)
+      nyear = gyear - NANAKSHAHI_EPOCH_IN_GREGORIAN.year
     else
-      nyear = gyear - 1468
+      nyear = gyear - (NANAKSHAHI_EPOCH_IN_GREGORIAN.year - 1)
     end
 
     nanakshahi_months = date_grid(gyear)
