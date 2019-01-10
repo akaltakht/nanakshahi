afe6860691edbb45fe1e146cdbe531aad3bfa411
Arvinder Singh <info@pippallabs.com>
2019-01-09 16:55:28 -0800

    🎨 Added yard comments for Date monkeypatching
     #4

--- lib/nanakshahi/date.rb
+++ lib/nanakshahi/date.rb
@@ -1,5 +1,9 @@
 # Monkey Patch Ruby Date object
+#
 class Date
+  # Adds conversion of date object to Nanakshahi in Ruby Date class
+  #
+  # @return [Nanakshahi] returns corresponding Nanakshahi date object after conversion
   def to_nanakshahi
     Nanakshahi.from_gregorian(self.year, self.mon, self.day)
   end
